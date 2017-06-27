69b498f244066a89333f301ca943d9c8c264cbaa
MinhCT <minhct1997@gmail.com>
2017-06-27 22:18:56 +0700

    - Add new plugin project: Nop.Plugin.Payments.BaoKim in folder Plugins

---
+++ Plugins/Nop.Plugin.Payments.BaoKim/BaoKimPluginMain.cs
@@ -0,0 +1,28 @@
+﻿using Nop.Services.Payments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nop.Core.Domain.Orders;
+using Nop.Core.Plugins;
+using System.Web.Routing;
+using Nop.Services.Common;
+
+namespace Nop.Plugin.Payments.BaoKim
+{
+    public class BaoKimPluginMain : BasePlugin, IMiscPlugin
+    {
+        /// <summary>
+        /// Gets a route for provider configuration
+        /// </summary>
+        /// <param name="actionName">Action name</param>
+        /// <param name="controllerName">Controller name</param>
+        /// <param name="routeValues">Route values</param>
+        public void GetConfigurationRoute(out string actionName, out string controllerName, out RouteValueDictionary routeValues)
+        {
+            actionName = "Configure";
+            controllerName = "PaymentBaoKimController";
+            routeValues = new RouteValueDictionary { { "Namespaces", "Nop.Plugin.Payments.BaoKim.Controllers" }, { "area", null } };
+        }
+    }
+}
---
+++ Plugins/Nop.Plugin.Payments.BaoKim/Controllers/PaymentBaoKimController.cs
@@ -0,0 +1,30 @@
+﻿using Nop.Web.Framework.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nop.Services.Payments;
+using System.Web.Mvc;
+
+namespace Nop.Plugin.Payments.BaoKim.Controllers
+{
+    public class PaymentBaoKimController : BasePaymentController
+    {
+        // Redirect request to appropriate view
+        public ActionResult Configure()
+        {
+            return View("~/Plugins/Payments.BaoKim/Views/Configure.cshtml");
+        }
+
+        public override ProcessPaymentRequest GetPaymentInfo(FormCollection form)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IList<string> ValidatePaymentForm(FormCollection form)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
